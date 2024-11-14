ed41c59cba2a8e6fa1d3bc7e8371b9fed1512143
Jakub <jakub@schwenkbeck.de>
2024-11-14 18:30:32 +0100

    Test with aho corasick graph works partly

--- StringPatternMatching/src/main/scala/StringPatternMatching.scala
+++ StringPatternMatching/src/main/scala/StringPatternMatching.scala
@@ -5,11 +5,12 @@
 // this file is the main entry point later on
 
 @main
-def main() : Unit = 
+def main() : Unit =
   println("nothing implemented yet")
   var TEST_States : Map[Int, State] = Map(0 -> State(0,Map("h" -> 1), false),1 -> State(1,null,true) )
   var TEST_SearchString : String = "This is her Testshe"
   var TEST_Keywords : List[String] = List("his", "her", "she")
-  var FSM = FiniteStateMachine(TEST_SearchString,TEST_Keywords,TEST_States)
+  var FSM = FiniteStateMachine(TEST_SearchString,TEST_Keywords,ahoCorasickGraph)
   println(s"${FSM.PMM()}")
 
+  print(ahoCorasickGraph)
