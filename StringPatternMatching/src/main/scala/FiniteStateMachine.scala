d45055ab734979e443eb209eeb09bd3d1c7ad422
Jakub <jakub@schwenkbeck.de>
2024-11-14 18:11:29 +0100

    Test implementation runs on empty output without error

--- StringPatternMatching/src/main/scala/FiniteStateMachine.scala
+++ StringPatternMatching/src/main/scala/FiniteStateMachine.scala
@@ -1,7 +1,7 @@
 // This class defines a finite state machine which pattern matches string inputs with the keywords
 
 
-case class State(ID : Integer , Successor : Map[String,Int],endState : Boolean) // all states have an own ID and a single successor which is mapped by its input, also known by its ID
+case class State(ID : Integer , Successor : Map[String,Int], endState : Boolean) // all states have an own ID and a single successor which is mapped by its input, also known by its ID
 
 
 
@@ -17,7 +17,7 @@ class FiniteStateMachine(SearchText: String, Keywords : List[String],InputStates
 
 
   def PMM() : List[Int] =
-    var Output : List[Int]  = List()
+    val Output: List[Int] = List()
     for (i <- 0 until keywords.length ){
       for (j <- 0 until keywords(i).length) {
         var gotoOutput: Int = goto(keywords(i).charAt(j).toString)
@@ -27,14 +27,16 @@ class FiniteStateMachine(SearchText: String, Keywords : List[String],InputStates
           currentStateID = gotoOutput
           val currentStateOpt: Option[State] = states.get(currentStateID)
            currentStateOpt match {
-             case Some(currentState) => if (currentState.endState){Output.appended(currentStateID)}
+             case Some(currentState) => if (currentState.endState){
+                Output :+ currentStateID
+             }
              case None =>
            }
 
         }
       }
     }
-      List(1,2,3)
+      Output
 
 
 
@@ -46,17 +48,18 @@ class FiniteStateMachine(SearchText: String, Keywords : List[String],InputStates
     currentStateOpt match {
       case Some(currentState) =>
         // Need to check if EndState!! --> output
-
-          // Access the Successor map of the current state
-          currentState.Successor.get(input) match {
-            case Some(nextStateID) =>
-              // Update the current state ID and move to the next state
-              currentStateID = nextStateID
-              nextStateID
-            case None =>
-              // If there's no matching input in the Successor map, tell PMM to call fail
-              -1
-          }
+          if(currentState.Successor != null) {
+            // Access the Successor map of the current state
+            currentState.Successor.get(input) match {
+              case Some(nextStateID) =>
+                // Update the current state ID and move to the next state
+                currentStateID = nextStateID
+                nextStateID
+              case None =>
+                // If there's no matching input in the Successor map, tell PMM to call fail
+                -1
+            }
+          } else {-1}
 
 
 
--- StringPatternMatching/src/main/scala/StringPatternMatching.scala
+++ StringPatternMatching/src/main/scala/StringPatternMatching.scala
@@ -5,4 +5,11 @@
 // this file is the main entry point later on
 
 @main
-def main() : Unit = println("nothing implemented yet")
+def main() : Unit = 
+  println("nothing implemented yet")
+  var TEST_States : Map[Int, State] = Map(0 -> State(0,Map("h" -> 1), false),1 -> State(1,null,true) )
+  var TEST_SearchString : String = "This is her Testshe"
+  var TEST_Keywords : List[String] = List("his", "her", "she")
+  var FSM = FiniteStateMachine(TEST_SearchString,TEST_Keywords,TEST_States)
+  println(s"${FSM.PMM()}")
+
