5bae1e81a1f33fa21f15574532a20025027b9b06
Jakub <jakub@schwenkbeck.de>
2024-11-14 17:06:30 +0100

    added pattern matching for Option of current state in the FSM goto func body

--- StringPatternMatching/src/main/scala/FiniteStateMachine.scala
+++ StringPatternMatching/src/main/scala/FiniteStateMachine.scala
@@ -1,33 +1,50 @@
 // This class defines a finite state machine which pattern matches string inputs with the keywords
 
+
 case class State(ID : Integer , Successor : Map[String,Int]) // all states have an own ID and a single successor which is mapped by its input, also known by its ID
-class FiniteStateMachine(SearchText: String, Keywords : List[String],InputStates : Map[Int, State]){
-
-  private var states : Map[Int, State] = InputStates
-  private var text : String = SearchText
-  private var keywords : List[String] = Keywords
 
 
-  private val currentStateID : Int = 0
-  def getCurrentStateID : Int = currentStateID
+class FiniteStateMachine(SearchText: String, Keywords : List[String],InputStates : Map[Int, State]) {
 
-  def goto(input : String) : Int =
-    val currentState = states.getOrElse(currentStateID,0)
-    if(currentState == 0){
-      ???
-      // fail function
-    }else {
-      //check input with successor map
+  private val states: Map[Int, State] = InputStates
+  private var text: String = SearchText
+  private var keywords: List[String] = Keywords
+
+
+  private var currentStateID: Int = 0
+
+  def getCurrentStateID: Int = currentStateID
+
+  def goto(input: String): Int =
+    val currentStateOpt: Option[State] = states.get(currentStateID)
+
+    currentStateOpt match {
+      case Some(currentState) =>
+        // Access the Successor map of the current state
+        currentState.Successor.get(input) match {
+          case Some(nextStateID) =>
+            // Update the current state ID and move to the next state
+            currentStateID = nextStateID
+            nextStateID
+          case None =>
+            // If there's no matching input in the Successor map, call the fail function
+            fail()
+            -1
+        }
+      case None =>
+        fail()
+        -1
     }
 
 
 
 
 
-    0
 
 
 
+  def fail(): Unit =
+    println("nothing")
 
 }
 
