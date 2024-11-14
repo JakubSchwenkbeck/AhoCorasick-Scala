1ff8298067ec395bb28aca7513137f693f342a3c
Jakub <jakub@schwenkbeck.de>
2024-11-14 17:14:30 +0100

    included endstate? as boolean of case class State

--- StringPatternMatching/src/main/scala/FiniteStateMachine.scala
+++ StringPatternMatching/src/main/scala/FiniteStateMachine.scala
@@ -1,7 +1,8 @@
 // This class defines a finite state machine which pattern matches string inputs with the keywords
 
 
-case class State(ID : Integer , Successor : Map[String,Int]) // all states have an own ID and a single successor which is mapped by its input, also known by its ID
+case class State(ID : Integer , Successor : Map[String,Int],endState : Boolean) // all states have an own ID and a single successor which is mapped by its input, also known by its ID
+
 
 
 class FiniteStateMachine(SearchText: String, Keywords : List[String],InputStates : Map[Int, State]) {
@@ -9,7 +10,6 @@ class FiniteStateMachine(SearchText: String, Keywords : List[String],InputStates
   private val states: Map[Int, State] = InputStates
   private var text: String = SearchText
   private var keywords: List[String] = Keywords
-
 
   private var currentStateID: Int = 0
 
@@ -20,17 +20,26 @@ class FiniteStateMachine(SearchText: String, Keywords : List[String],InputStates
 
     currentStateOpt match {
       case Some(currentState) =>
-        // Access the Successor map of the current state
-        currentState.Successor.get(input) match {
-          case Some(nextStateID) =>
-            // Update the current state ID and move to the next state
-            currentStateID = nextStateID
-            nextStateID
-          case None =>
-            // If there's no matching input in the Successor map, call the fail function
-            fail()
-            -1
+        // Need to check if EndState!! --> output
+        if (currentState.endState) {
+          println(s"Reached endState at ${currentStateID}")
+          0}
+        else {
+
+          // Access the Successor map of the current state
+          currentState.Successor.get(input) match {
+            case Some(nextStateID) =>
+              // Update the current state ID and move to the next state
+              currentStateID = nextStateID
+              nextStateID
+            case None =>
+              // If there's no matching input in the Successor map, call the fail function
+              fail()
+              -1
+          }
+
         }
+
       case None =>
         fail()
         -1
