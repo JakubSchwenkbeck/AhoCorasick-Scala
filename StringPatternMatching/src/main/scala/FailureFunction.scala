9905742ed8bad6fa0b84d83416b3e64cfe7e7471
Jakub <jakub@schwenkbeck.de>
2024-11-18 22:48:48 +0100

    started to implement the 'computeFailure' function which generates the Map for Failure states

--- StringPatternMatching/src/main/scala/FailureFunction.scala
+++ StringPatternMatching/src/main/scala/FailureFunction.scala
@@ -1,5 +1,9 @@
 
 
 
-def generateFailureFunction() : Unit =
-    
+
+
+def computeFail(): Map[Int, Int] = {
+  // Initialize fail function, setting all states to fail to root (state 0)
+  var fail = Map[Int, Int]().withDefaultValue(0)
+  val queue = scala.collection.mutable.Queue[Int]()
