03ee532115238ebe3856ea8e30e62440e4f64822
Jakub <jakub@schwenkbeck.de>
2024-11-25 15:30:18 +0100

    added simple comments to later be scaladoc

--- StringPatternMatching/src/main/scala/GUI/VisualizeTrie.scala
+++ StringPatternMatching/src/main/scala/GUI/VisualizeTrie.scala
@@ -3,39 +3,45 @@ import Main.State
 import processing.core.PApplet
 
 class VisualizeTrie(trie: Map[Int,State]) extends  PApplet  {
+  // Processing Settings:
   override def settings(): Unit = {
-    size(1800, 1000) // Fenstergröße
+    size(1200, 1000) // Windowsize
   }
 
   override def setup(): Unit = {
-    background(255) // Weißer Hintergrund
-    noLoop()
+    background(255) // white backgorund
+    noLoop() // not looping, as picture is static
   }
 
   override def draw(): Unit = {
     visTrie()
   }
+
+  // fields of class:
   private val Trie : Map[Int, State] = trie
   private val first: State = Trie(0)
   private var x = 40
   private var y = 40
 
-  def visTrie(): Unit=
+
+  // main handler function which calls helper functions
+  private def visTrie(): Unit=
 
       // Draw first trie as circle!
 
-    drawState(first,x,y)
+    drawState(first,x,y) // init State
+
     if (first.Successor.nonEmpty) {
       for (elem <- first.Successor) {
         // for each State in our trie,
-
-
         // Draw the State and recurse on its successors
         recTrie(Trie(elem._2))
 
       }
     }
     println(Trie)
+
+    // recursive function to make a BFS like rec on the Trie and draw all states accordingly
   private def recTrie(st : State) : Unit =
     // draw s, rec on Successors
     if (st != null) {
@@ -55,6 +61,8 @@ class VisualizeTrie(trie: Map[Int,State]) extends  PApplet  {
       }
     }
 
+
+  // drawing function for a state at given pos
   private def drawState(st: State, xpos : Int, ypos :Int): Unit ={
 
       val ID = st.ID
