4b6240f3e4aafd8d75dacca2ea06d2fe17b21c48
Jakub <jakub@schwenkbeck.de>
2024-11-29 19:49:28 +0100

    added more unit tests for the PPM

--- StringPatternMatching/src/test/scala/FiniteStateMachineTest.scala
+++ StringPatternMatching/src/test/scala/FiniteStateMachineTest.scala
@@ -65,4 +65,57 @@ class FiniteStateMachineTest extends AnyFlatSpec with Matchers {
     val fsm2 = FiniteStateMachine(searchStringEmpty, keywords2)
     fsm2.PMM() shouldEqual List()
   }
+  it should "PMM should correctly match patterns in the text" in {
+    val fsm = new FiniteStateMachine("abcabc", List("ab", "bc"))
+    val matches : List[(Int,String)] = fsm.PMM()
+
+
+    matches.exists(m => m._1 == 2 && m._2 == "ab") shouldEqual true // Match found at index 2 for "ab"
+    }
+  it should "return no matches for non-matching patterns" in {
+    val fsm = new FiniteStateMachine("abcdefgh", List("xyz", "pq"))
+    val matches: List[(Int, String)] = fsm.PMM()
+
+  }
+
+  it should "correctly match single-character patterns" in {
+    val fsm = new FiniteStateMachine("abcabc", List("a", "b", "c"))
+    val matches: List[(Int, String)] = fsm.PMM()
+
+    // Checking if the matches are found at the correct positions
+
+    matches.exists(m => m._1 == 3 && m._2 == "c") shouldEqual true // Match found at index 6 for "c"
+  }
+
+  it should "handle empty search text gracefully" in {
+    val fsm = new FiniteStateMachine("", List("ab", "bc"))
+    val matches: List[(Int, String)] = fsm.PMM()
+
+    matches.isEmpty shouldEqual true // No matches since the search text is empty
+  }
+
+  it should "handle empty keywords list gracefully" in {
+    val fsm = new FiniteStateMachine("abcabc", List())
+    val matches: List[(Int, String)] = fsm.PMM()
+
+    matches.isEmpty shouldEqual true // No matches since the keywords list is empty
+  }
+
+  it should "return matches for patterns at the beginning of the text" in {
+    val fsm = new FiniteStateMachine("abcrdefgh", List("abc", "def"))
+    val matches: List[(Int, String)] = fsm.PMM()
+
+
+    matches.exists(m => m._1 == 3 && m._2 == "abc") shouldEqual true // Match for "def" at index 6
+  }
+
+  it should "correctly handle overlapping patterns" in {
+    val fsm = new FiniteStateMachine("abcabcabc", List("ab", "bc", "abc"))
+    val matches: List[(Int, String)] = fsm.PMM()
+
+    matches.exists(m => m._1 == 8 && m._2 == "ab") shouldEqual true // Match for "abc" at index 7
+  }
+
 }
+
+
