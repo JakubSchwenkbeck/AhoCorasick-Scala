c9c6f6ee4d277efa308ff363f03cb88c06e45e25
Jakub <jakub@schwenkbeck.de>
2024-11-30 18:04:27 +0100

    dummy file for later implementation

---
+++ StringPatternMatching/src/main/scala/GUI/RealWorldApplications/DNASequenceAnalysis.scala
@@ -0,0 +1,17 @@
+/*package GUI.RealWorldApplications
+
+import processing.core.PApplet
+
+
+  // Define DNA sequences to search for (patterns)
+  val genePatterns = List("ATG", "TGC", "CGA", "AAC")
+  val dnaSequence = """ATGAGTCGTGCAAATCGATGCGTTACAGGTCGAAGTACGATAACAGTGCATCGT"""
+
+
+  def displayDNAAnalysisExample(parent: PApplet): Unit = {
+    parent.text("DNA Sequence Analysis using Aho-Corasick:", 20, 30)
+    parent.textSize(12)
+    parent.text("We use Aho-Corasick to search for specific gene patterns in a DNA sequence.", 20, 60)
+
+
+*/
